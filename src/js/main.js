64cf963f6728241aeaa663868d44508915a33afa
Ioann <vanes.sysoev@gmail.com>
2021-06-29 13:27:39 +0300

    feat: actions
    actions( find, eq, closest, html, siblings, index)

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,13 @@ import $ from './lib/lib';
 
 
 $('button').on('click', function() {
-    $(this).toggleClass('active');
+    $('div').eq(2).toggleClass('active');
 });
 
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findmeq'));
+console.log($('.findme').siblings());
