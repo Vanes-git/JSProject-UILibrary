63bbecaf9dee6486ed7064a7bb434ba74878b688
Ioann <vanes.sysoev@gmail.com>
2021-06-30 14:29:05 +0300

    feat: effect(Toggle)

--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -1,10 +1,10 @@
-import $ from '../core';
+import $ from "../core";
 
-$.prototype.animateOverTime = function(dur, cb, fin) {
+$.prototype.animateOverTime = function (dur, cb, fin) {
     let timeStart;
 
     function _animateOverTime(time) {
-        if(!timeStart) {
+        if (!timeStart) {
             timeStart = time;
         }
 
@@ -13,47 +13,65 @@ $.prototype.animateOverTime = function(dur, cb, fin) {
 
         cb(complection);
 
-        if(timeElapsed < dur) {
+        if (timeElapsed < dur) {
             requestAnimationFrame(_animateOverTime);
         } else {
-            if(typeof fin === 'function') {
+            if (typeof fin === "function") {
                 fin();
             }
         }
-    };
+    }
 
     return _animateOverTime;
 };
 
-$.prototype.fadeIn = function(dur, display = 'block', fin) {
+$.prototype.fIn = function(i, display, dur, fin) {
+    this[i].style.display = display || "block";
+
+    const _fadeIn = (complection) => {
+        this[i].style.opacity = complection;
+    };
+
+    const ani = this.animateOverTime(dur, _fadeIn, fin);
+    requestAnimationFrame(ani);
+};
+
+$.prototype.fOut = function(i, display, dur, fin)  {
+    const _fadeOut = (complection) => {
+        this[i].style.opacity = 1 - complection;
+        if (complection === 1) {
+            this[i].style.display = "none";
+        }
+    };
+
+    const ani = this.animateOverTime(dur, _fadeOut, fin);
+    requestAnimationFrame(ani);
+};
+
+$.prototype.fadeIn = function (dur, display, fin) {
     for (let i = 0; i < this.length; i++) {
-        this[i].style.display = display;
-
-        const _fadeIn = (complection) => {
-            this[i].style.opacity = complection;
-        };
-
-        const ani = this.animateOverTime(dur, _fadeIn, fin);
-        requestAnimationFrame(ani);
+        this.fIn(i, display, dur, fin);
     }
 
     return this;
-}
+};
 
-$.prototype.fadeOut = function(dur, fin) {
+$.prototype.fadeOut = function (dur, fin) {
     for (let i = 0; i < this.length; i++) {
-        
-
-        const _fadeOut = (complection) => {
-            this[i].style.opacity = 1 - complection;
-            if(complection === 1){
-                this[i].style.display = 'none';
-            }            
-        };
-
-        const ani = this.animateOverTime(dur, _fadeOut, fin);
-        requestAnimationFrame(ani);
+        this.fOut(i, display, dur, fin);
     }
 
     return this;
-}
+};
+
+$.prototype.fadeToggle = function (dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
+        if (window.getComputedStyle(this[i]).display === "none") {
+            this.fIn(i, display, dur, fin);
+        } else {
+            this.fOut(i, display, dur, fin);
+        }
+    }
+
+    return this;
+};
